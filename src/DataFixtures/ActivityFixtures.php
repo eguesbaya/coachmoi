0669254ffe640cee196f1395be985ec9bfb4fedd
eguesbaya <guesbaya@hotmail.fr>
2021-06-04 10:49:32 +0200

    removed json files. created activity fixture.

--- src/DataFixtures/ActivityFixtures.php
+++ src/DataFixtures/ActivityFixtures.php
@@ -4,13 +4,19 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Activity;
 
 class ActivityFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 1; $i <= 50; $i++) {
+            $activity = new Activity();
+            $activity->setName('Activité ' . $i);
+            $activity->setDescription('Une description de l\'activité ' . $i);
+
+            $manager->persist($activity);
+        }
 
         $manager->flush();
     }
