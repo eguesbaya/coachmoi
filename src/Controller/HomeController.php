e2f0720eb729464991d0702639e6d9e936bf73af
eguesbaya <guesbaya@hotmail.fr>
2021-06-04 17:01:31 +0200

    Removed commented code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -18,15 +18,6 @@ class HomeController extends AbstractController
         $activities = $this->getDoctrine()
             ->getRepository(Activity::class)
             ->findAll();
-        /* fake table, remove once db is created
-        $activities = [
-            ['name' => 'Yoga', 'description' => 'On respire'],
-            ['name' => 'Remise en forme', 'description' => 'Allez courage'],
-            ['name' => 'Escalade', 'description' => 'Regarde pas en bas!'],
-            ['name' => 'Salsa', 'description' => 'Bailemos'],
-            ['name' => 'Judo', 'description' => 'Ceinture noire'],
-            ['name' => 'Boxe', 'description' => 'Flot like a butterfly, sting like a bee'],
-        ];*/
 
         return $this->render('home/index.html.twig', ['activities' => $activities]);
     }
