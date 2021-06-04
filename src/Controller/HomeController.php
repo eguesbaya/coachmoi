922ea8df00375663e82106d32e0845e62c7641b8
eguesbaya <guesbaya@hotmail.fr>
2021-06-04 16:59:44 +0200

    Lien établi avec ActivityFixtures

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Activity;
 
 class HomeController extends AbstractController
 {
@@ -13,7 +14,11 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        /* fake table, remove once db is created*/
+
+        $activities = $this->getDoctrine()
+            ->getRepository(Activity::class)
+            ->findAll();
+        /* fake table, remove once db is created
         $activities = [
             ['name' => 'Yoga', 'description' => 'On respire'],
             ['name' => 'Remise en forme', 'description' => 'Allez courage'],
@@ -21,7 +26,7 @@ class HomeController extends AbstractController
             ['name' => 'Salsa', 'description' => 'Bailemos'],
             ['name' => 'Judo', 'description' => 'Ceinture noire'],
             ['name' => 'Boxe', 'description' => 'Flot like a butterfly, sting like a bee'],
-        ];
+        ];*/
 
         return $this->render('home/index.html.twig', ['activities' => $activities]);
     }
