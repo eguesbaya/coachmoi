d8814f8d9a7fa51706130a3b2aa425dd46b8957f
eguesbaya <guesbaya@hotmail.fr>
2021-06-09 10:09:39 +0200

    Implemented sylvain's remarks

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,19 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Activity;
+use App\Repository\ActivityRepository;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(ActivityRepository $activityRepository): Response
     {
-
-        $activities = $this->getDoctrine()
-            ->getRepository(Activity::class)
-            ->findAll();
-
+        $activities = $activityRepository->findAll();
         return $this->render('home/index.html.twig', ['activities' => $activities]);
     }
 }
