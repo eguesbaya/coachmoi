0f7198aa440ec66ab9a10245cab2c5d1a69348d5
Khadim <khadim.bousso@gmail.com>
2021-06-09 11:29:40 +0200

    fix Home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,17 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Coach;
+use App\Repository\CoachRepository;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(CoachRepository $coachRepository): Response
     {
-        $coachs = $this->getDoctrine()
-            ->getRepository(Coach::class)
-            ->findAll();
+        $coachs = $coachRepository->findAll();
 
         return $this->render('home/index.html.twig', [
             'coachs' => $coachs,
