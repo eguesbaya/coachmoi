2ff1226cf34150dee3ba7bff2622dca7ba711254
Khadim <khadim.bousso@gmail.com>
2021-06-10 10:54:27 +0200

    fix error with date

--- migrations/Version20210609173116.php
+++ migrations/Version20210609173116.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210608143159 extends AbstractMigration
+final class Version20210609173116 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20210608143159 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE activity ADD is_featured TINYINT(1) NOT NULL');
+        $this->addSql('CREATE TABLE training_space (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, photo VARCHAR(255) DEFAULT NULL, description LONGTEXT DEFAULT NULL, address VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE activity DROP is_featured');
+        $this->addSql('DROP TABLE training_space');
     }
 }
