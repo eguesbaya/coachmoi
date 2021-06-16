71d500aecbbc1a471cd24ce14e65af40d206e850
Serdar45000 <serdar45000@gmail.com>
2021-06-16 15:03:41 +0200

    Envoie du message & première mise en forme de l'email

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,26 +4,34 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact(Request $request): Response
+    public function contact(Request $request, MailerInterface $mailer): Response
     {
         $contact = new Contact();
         $contactForm = $this->createForm(ContactType::class, $contact);
         $contactForm->handleRequest($request);
 
         if ($contactForm->isSubmitted() && $contactForm->isValid()) {
+            $email = (new Email())
+            ->from('your_email@example.com')
+            ->from($this->getParameter('mailer_from'))
+            ->to('serdar45000@gmail.com')
+            ->subject('Nouveau message du site COACH MOI')
+            ->html($this->renderView('contact/emailSendMessage.html.twig', ['contact' => $contact]));
+            $mailer->send($email);
             return $this->render('contact/messageSent.html.twig');
         }
-
         return $this->render('contact/index.html.twig', [
             'contact' => $contactForm->createView()
         ]);
