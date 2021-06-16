c8a5800754a7483fa5c2275d37273b1823fa56a1
eguesbaya <guesbaya@hotmail.fr>
2021-06-16 23:02:31 +0200

    created admin client view structure

---
+++ src/Form/ClientType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Client;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ClientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('birthdate')
+            ->add('address')
+            ->add('goal')
+            ->add('budget')
+            ->add('groupSize')
+            ->add('isApt')
+            ->add('user')
+            ->add('activity')
+            ->add('practiceLevel')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Client::class,
+        ]);
+    }
+}
