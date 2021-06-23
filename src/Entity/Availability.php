71caa0fed27e9d65dabad4add617a7abce3e839c
Thuydieutran <thuy.dieutran86@gmail.com>
2021-06-23 16:51:09 +0200

    Formulaire de la partie Disponibilité

---
+++ src/Entity/Availability.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AvailabilityRepository::class)
+ */
+class Availability
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Weekday::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $weekday;
+
+    /**
+    * @ORM\Column(type="time")
+    * A "H:i" formatted value
+    */
+    private $startTime;
+
+    /**
+    * @ORM\Column(type="time")
+    * A "H:i" formatted value
+    * @Assert\Expression(
+    * "this.getStartTime() < this.getEndTime()",
+    * message="L'heure de fin ne doit pas être antérieure à l'heure début"
+    * )
+    */
+    private $endTime;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getWeekday(): ?Weekday
+    {
+        return $this->weekday;
+    }
+
+    public function setWeekday(?Weekday $weekday): self
+    {
+        $this->weekday = $weekday;
+
+        return $this;
+    }
+
+    public function getStartTime(): ?\DateTimeInterface
+    {
+        return $this->startTime;
+    }
+
+    public function setStartTime(\DateTimeInterface $startTime): self
+    {
+        $this->startTime = $startTime;
+
+        return $this;
+    }
+
+    public function getEndTime(): ?\DateTimeInterface
+    {
+        return $this->endTime;
+    }
+
+    public function setEndTime(\DateTimeInterface $endTime): self
+    {
+        $this->endTime = $endTime;
+
+        return $this;
+    }
+}
---
+++ src/Form/AvailabilityType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Weekday;
+use App\Entity\Availability;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+
+class AvailabilityType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('weekday', EntityType::class, [
+                'label' => 'Jour',
+                'class' => Weekday::class,
+                'choice_label' => 'name',
+                'choice_translation_domain' => true
+            ])
+            ->add('startTime', TimeType::class, [
+                'label' => 'Heure de début',
+                'widget' => 'single_text',
+            ])
+            ->add('endTime', TimeType::class, [
+                'label' => 'Heure de fin',
+                'widget' => 'single_text',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+            'data_class' => Availability::class,
+        ]);
+    }
+}
