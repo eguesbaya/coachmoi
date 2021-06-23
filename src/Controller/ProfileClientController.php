9f165b4c565e301b369a259b2510b45e2afd5846
eguesbaya <guesbaya@hotmail.fr>
2021-06-23 20:48:23 +0200

    Created view for Mes informations linked to User

--- src/Controller/ProfileClientController.php
+++ src/Controller/ProfileClientController.php
@@ -2,19 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfileClientController extends AbstractController
 {
     /**
      * @Route("/profile/client", name="profile_client")
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+        $user = $this->getUser();
+
         return $this->render('profile_client/index.html.twig', [
-            'controller_name' => 'ProfileClientController',
+            'user' => $user,
         ]);
     }
 }
