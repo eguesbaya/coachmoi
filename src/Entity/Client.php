1fd1025ecaca0661ac0c1b04cafc24900dd2f5b1
eguesbaya <guesbaya@hotmail.fr>
2021-06-24 17:22:15 +0200

    added fields in Client

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -19,6 +19,26 @@ class Client
 
     private User $user;
 
+    /**
+     * @ORM\Column(type="text")
+     */
+    private string $goal;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private int $budget;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private int $groupSize;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private bool $isApt;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -32,6 +52,54 @@ class Client
     public function setUser(User $user): self
     {
         $this->user = $user;
+
+        return $this;
+    }
+
+    public function getGoal(): ?string
+    {
+        return $this->goal;
+    }
+
+    public function setGoal(string $goal): self
+    {
+        $this->goal = $goal;
+
+        return $this;
+    }
+
+    public function getBudget(): ?int
+    {
+        return $this->budget;
+    }
+
+    public function setBudget(int $budget): self
+    {
+        $this->budget = $budget;
+
+        return $this;
+    }
+
+    public function getGroupSize(): ?int
+    {
+        return $this->groupSize;
+    }
+
+    public function setGroupSize(int $groupSize): self
+    {
+        $this->groupSize = $groupSize;
+
+        return $this;
+    }
+
+    public function getIsApt(): ?bool
+    {
+        return $this->isApt;
+    }
+
+    public function setIsApt(bool $isApt): self
+    {
+        $this->isApt = $isApt;
 
         return $this;
     }
