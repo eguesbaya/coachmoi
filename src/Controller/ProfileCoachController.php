7a27d39448d4e4d4d58d1df6235050fd2272a155
Serdar45000 <serdar45000@gmail.com>
2021-07-02 00:31:44 +0200

    Préférences acitivités du coach

--- src/Controller/ProfileCoachController.php
+++ src/Controller/ProfileCoachController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CoachRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfileCoachController extends AbstractController
 {
     /**
      * @Route("/profile/coach", name="profile_coach")
      */
-    public function index(): Response
+    public function index(CoachRepository $coach): Response
     {
+
         return $this->render('profile_coach/index.html.twig');
     }
 }
