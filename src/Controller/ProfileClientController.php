cbbc5e13477a9f9c2917e054579dcd9180681098
eguesbaya <guesbaya@hotmail.fr>
2021-07-02 11:09:46 +0200

    removed irrelevant code

--- src/Controller/ProfileClientController.php
+++ src/Controller/ProfileClientController.php
@@ -26,24 +26,4 @@ class ProfileClientController extends AbstractController
     {
         return $this->render('profile_client/index.html.twig');
     }
-
-        /**
-     * @Route("/profile/client/edit", name="profile_client_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Client $client): Response
-    {
-        $form = $this->createForm(ClientType::class, $client);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('test_client_profile_index');
-        }
-
-        return $this->render('test_client_profile/edit.html.twig', [
-            'client' => $client,
-            'form' => $form->createView(),
-        ]);
-    }
 }
