1dd4b21776f51700d394ae34ecbababbee97d7df
Thuydieutran <thuy.dieutran86@gmail.com>
2021-07-05 23:43:40 +0200

    Show fiche réservation sans section coach-moi

--- src/Controller/CoachBookingController.php
+++ src/Controller/CoachBookingController.php
@@ -3,27 +3,38 @@
 namespace App\Controller;
 
 use App\Entity\CoachBooking;
-use App\Form\CoachBookingType;
 use App\Repository\CoachBookingRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/demandes")
+ * @isGranted("ROLE_SUPER_ADMIN")
  */
 class CoachBookingController extends AbstractController
 {
     /**
      * @Route("/", name="coach_booking_index", methods={"GET"})
-     * @isGranted("ROLE_SUPER_ADMIN")
      */
     public function index(CoachBookingRepository $coachBookingRepo): Response
     {
         return $this->render('coach_booking/index.html.twig', [
             'coach_bookings' => $coachBookingRepo->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/{coach_booking_id}", name="coach_booking_show", methods={"GET", "POST"})
+     * @ParamConverter("coachBooking", class="App\Entity\CoachBooking", options={"mapping": {"coach_booking_id": "id"}})
+     */
+    public function show(CoachBooking $coachBooking): Response
+    {
+        return $this->render('coach_booking/show.html.twig', [
+            'coach_booking' => $coachBooking
         ]);
     }
 }
