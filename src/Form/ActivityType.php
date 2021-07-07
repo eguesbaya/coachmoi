b065329785ec81bba2901d945e93ec6993c7bddc
Serdar45000 <serdar45000@gmail.com>
2021-07-07 21:56:35 +0200

    Ajout activité Super Admin

---
+++ src/Form/ActivityType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Activity;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class ActivityType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => "Activité :"
+            ])
+            ->add('description', TextType::class, [
+                'label' => "Description :"
+            ])
+            ->add('isFeatured', ChoiceType::class, [
+                'label' => 'Actif',
+                'choices' => [
+                    'actif' => true,
+                    'inactif' => false
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Activity::class,
+        ]);
+    }
+}
