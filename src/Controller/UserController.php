758a696130e344845422567131bb4985ff75400c
eguesbaya <guesbaya@hotmail.fr>
2021-07-12 23:29:23 +0200

    fixed userController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -32,7 +32,7 @@ class UserController extends AbstractController
     public function new(Request $request): Response
     {
         $user = new User();
-        $form = $this->createForm(User1Type::class, $user);
+        $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
