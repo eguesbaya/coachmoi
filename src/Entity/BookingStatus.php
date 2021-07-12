f45fedae127ba7f806760573670d616174fd2267
Thuydieutran <thuy.dieutran86@gmail.com>
2021-07-12 09:52:47 +0200

    Validate the booking status form

--- src/Entity/BookingStatus.php
+++ src/Entity/BookingStatus.php
@@ -6,6 +6,7 @@ use App\Repository\BookingStatusRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=BookingStatusRepository::class)
@@ -21,6 +22,7 @@ class BookingStatus
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Choice(callback="getStatus")
      */
     private ?string $status;
 
