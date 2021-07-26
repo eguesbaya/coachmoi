6cb9599d56a5e9eacc4f7aefffe8d10c7dfcfa00
eguesbaya <guesbaya@hotmail.fr>
2021-07-26 13:43:49 +0200

    role utilisateur préselectionné quand on veut l'éditer

--- src/Form/EditUserType.php
+++ src/Form/EditUserType.php
@@ -38,41 +38,9 @@ class EditUserType extends AbstractType
                 'expanded' => true,
                 'mapped' => false,
                 'multiple' => false,
-                // 'data' => $user->getRole()
+                'data' => $builder->getData()->getRoles()[0]
             ])
             ->add('Valider', SubmitType::class);
-
-    //     $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
-    //         $user = $event->getData();
-    //         $form = $event->getForm();
-
-    //         $form->add('email', EmailType::class, [
-    //             'constraints' => [
-    //                 new NotBlank([
-    //                    'message' => 'Merci de saisir une address email'
-    //                 ])
-    //                 ],
-    //                 'required' => true,
-    //                 'attr' => [
-    //                     'class' => 'form-control'
-    //                 ]
-    //         ]);
-
-    //          $form->add('roles', ChoiceType::class, [
-    //             'choices' => [
-    //                 'Client' => 'ROLE_CLIENT',
-    //                 'Coach' => 'ROLE_COACH',
-    //                 'SuperAdmin' => 'ROLE_SUPERADMIN',
-    //             ],
-    //             'expanded' => true,
-    //             'mapped' => false,
-    //             'multiple' => false,
-    //             'data' => $user->getRole() ? $user->getRole() : 0
-    //         ])
-
-    //         ->add('Valider', SubmitType::class);
-    //     })
-
         ;
     }
 
