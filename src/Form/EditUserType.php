2949eef28332ba8e3d1167b7040d9ca1c0e3769a
eguesbaya <guesbaya@hotmail.fr>
2021-07-26 11:56:43 +0200

    started but not functional

--- src/Form/EditUserType.php
+++ src/Form/EditUserType.php
@@ -3,13 +3,14 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class EditUserType extends AbstractType
 {
@@ -27,6 +28,7 @@ class EditUserType extends AbstractType
                         'class' => 'form-control'
                     ]
             ])
+
             ->add('roles', ChoiceType::class, [
                 'choices' => [
                     'Client' => 'ROLE_CLIENT',
@@ -36,9 +38,41 @@ class EditUserType extends AbstractType
                 'expanded' => true,
                 'mapped' => false,
                 'multiple' => false,
-                'label' => 'Rôles'
+                // 'data' => $user->getRole()
             ])
-            ->add('Valider', SubmitType::class)
+            ->add('Valider', SubmitType::class);
+
+    //     $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+    //         $user = $event->getData();
+    //         $form = $event->getForm();
+
+    //         $form->add('email', EmailType::class, [
+    //             'constraints' => [
+    //                 new NotBlank([
+    //                    'message' => 'Merci de saisir une address email'
+    //                 ])
+    //                 ],
+    //                 'required' => true,
+    //                 'attr' => [
+    //                     'class' => 'form-control'
+    //                 ]
+    //         ]);
+
+    //          $form->add('roles', ChoiceType::class, [
+    //             'choices' => [
+    //                 'Client' => 'ROLE_CLIENT',
+    //                 'Coach' => 'ROLE_COACH',
+    //                 'SuperAdmin' => 'ROLE_SUPERADMIN',
+    //             ],
+    //             'expanded' => true,
+    //             'mapped' => false,
+    //             'multiple' => false,
+    //             'data' => $user->getRole() ? $user->getRole() : 0
+    //         ])
+
+    //         ->add('Valider', SubmitType::class);
+    //     })
+
         ;
     }
 
